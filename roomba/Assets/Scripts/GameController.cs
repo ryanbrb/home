762bbdad44160c3e5e11ca7f85d295b2120bf5ab
Michael Pelz <mpelz.lill@gmail.com>
2019-01-26 13:24:03 -0800

    GameController and CSharp.

---
+++ roomba/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public float Coutnter;
+	protected static int _Level;
+	public static int Level
+	{
+		get
+		{
+			return _Level;
+		}
+		set
+		{
+			if (value > 0 && value < 5)
+				_Level = value;
+			return;
+		}
+	}
+
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public static void Win()
+	{
+	}
+	public static void Lose()
+	{
+	}
+}
