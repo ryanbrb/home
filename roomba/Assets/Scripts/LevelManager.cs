73e19b3773ec6eb1088880c4534524e5666b1270
Michael Pelz <mpelz.lill@gmail.com>
2019-01-26 15:54:58 -0800

    Update with empty functions for Mike for calling events.

--- roomba/Assets/Scripts/LevelManager.cs
+++ roomba/Assets/Scripts/LevelManager.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+public enum GameEvent {
+BatterDead, Warning
+}
+
+
 public class LevelManager : MonoBehaviour
 {
-    public string[] Level;
-
-    public void LoadRoom(int RoomNumber){
+	public string[] Level;
+	//public static LevelManager ThisisMe;
+	public void Start()
+	{
+		//LevelManager.ThisisMe = this;
+	}
+	public static void CallEvent(GameEvent EventCalled)
+	{
+		
+	}
+	
+	public void LoadRoom(int RoomNumber){
         if(Level[RoomNumber] != null) {
             SceneManager.LoadScene(Level[RoomNumber]);
             Debug.Log("Loading " + Level[RoomNumber]);
