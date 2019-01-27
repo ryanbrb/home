50cacacec64b7a15773b47c4465c054bc09a85e4
Michael Pelz <mpelz.lill@gmail.com>
2019-01-26 21:00:02 -0800

    Moonwalking cat

--- roomba/Assets/Scripts/Cat.cs
+++ roomba/Assets/Scripts/Cat.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Cat : MonoBehaviour
 {
     private float speed;
+	private Vector3 myScale;
+	
+	
     // Start is called before the first frame update
     void Start()
     {
         speed = 1.0f;
+		myScale = transform.localScale;
+		myScale.x = myScale.x * -1;
     }
 
     // Update is called once per frame
@@ -19,6 +24,18 @@ public class Cat : MonoBehaviour
 
     public void MoveDiagonal(Vector3 v3)
     {
+		if(v3.x > 0)
+		{
+			Vector3 newScale = myScale;
+			//newScale.x = newScale.x * -1;
+			transform.localScale = newScale;
+		}
+		else if(v3.x < 0)
+		{
+			Vector3 newScale = myScale;
+			newScale.x = newScale.x * -1;
+			transform.localScale = newScale;
+		}
         transform.Translate(speed * v3.normalized * Time.deltaTime);
     }
 }
