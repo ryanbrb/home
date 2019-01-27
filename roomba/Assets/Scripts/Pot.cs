d1e62e8eefa01a0946f5be5fef52e1312e9e72dc
Michael Pelz <mpelz.lill@gmail.com>
2019-01-26 19:21:56 -0800

    Pot and Dirt untested code.

---
+++ roomba/Assets/Scripts/Pot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+	private float fade = 3;
+	private bool knocked = false;
+	[SerializeField]
+	private SpriteRenderer sr;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(knocked)
+		{
+			fade = fade - Time.deltaTime;
+			sr.color = new Color(1, 1, 1, fade);
+			if (fade < 0)
+				Destroy(this);
+		}
+    }
+	private void KnockOver()
+	{
+		knocked = true;	
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Cat"))
+		{
+			KnockOver();
+		}
+	}
+}
