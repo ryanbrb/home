876873451abcef330e0dde8bc1697d950b8dfce7
Michael Pelz <mpelz.lill@gmail.com>
2019-01-26 20:10:41 -0800

    Dirt, pot, and cat collisions
    Got some good stuff working.

--- roomba/Assets/Scripts/Dirt.cs
+++ roomba/Assets/Scripts/Dirt.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class Dirt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Bounds MyBounds;
+	private Bounds RBounds;
+	// Start is called before the first frame update
+	void Start()
+	{
+		MyBounds = this.GetComponent<Collider2D>().bounds;
+		RBounds = MyBounds;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		    
-    }
-	
+	// Update is called once per frame
+	void Update()
+	{
+		if (RBounds != MyBounds)
+		{
+			if (BoundsContainedPercentage(MyBounds, RBounds) > 0.999f)
+			{
+				Destroy(this);
+			}
+		}
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Roomba"))
+		{
+			RBounds = collision.collider.bounds;
+		}
+	}
+
+	//region is the larger
+	public float BoundsContainedPercentage(Bounds obj, Bounds region)
+	{
+		var total = 1f;
+		for (var i = 0; i < 2; i++)
+		{
+			var dist = obj.min[i] > region.center[i] ?
+				obj.max[i] - region.max[i] :
+				region.min[i] - obj.min[i];
+			total *= Mathf.Clamp01(1f - dist / obj.size[i]);
+		}
+		return total;
+	}
 }
--- roomba/Assets/Scripts/Pot.cs
+++ roomba/Assets/Scripts/Pot.cs
@@ -6,26 +6,49 @@ public class Pot : MonoBehaviour
 {
 	private float fade = 3;
 	private bool knocked = false;
+	private Color StartColor;
 	[SerializeField]
 	private SpriteRenderer sr;
+	[SerializeField]
+	private GameObject myDirt;
 
-    // Update is called once per frame
-    void Update()
+	private void Start()
+	{
+		myDirt.active = false;
+		StartColor = sr.color;
+	}
+	// Update is called once per frame
+	void Update()
     {
         if(knocked)
 		{
 			fade = fade - Time.deltaTime;
-			sr.color = new Color(1, 1, 1, fade);
+			StartColor.a = fade;
+			sr.color = StartColor;// new Color(1, 1, 1, fade);
 			if (fade < 0)
-				Destroy(this);
+			{
+				gameObject.GetComponent<Collider2D>().enabled = false;
+				Destroy(this.gameObject);
+			}
 		}
     }
 	private void KnockOver()
 	{
-		knocked = true;	
+		knocked = true;
+		myDirt.active = true;
+		myDirt.transform.parent = null;
 	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
+		Debug.Log("Collider Entered for pot");
+		if (collision.gameObject.CompareTag("Cat"))
+		{
+			KnockOver();
+		}
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log("Trigger Entered for pot");
 		if (collision.gameObject.CompareTag("Cat"))
 		{
 			KnockOver();
