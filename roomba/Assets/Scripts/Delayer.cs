5f87d15485530dc4295928cb32599b3481eee97f
Finnark <kokaltulah@gmail.com>
2019-01-27 13:17:37 -0800

    Added retry UI and script

---
+++ roomba/Assets/Scripts/Delayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delayer : MonoBehaviour
+{
+
+	protected static Delayer Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance == null) {
+			Instance = this;
+		} else {
+			Destroy(this.gameObject);
+		}
+    }
+
+	public static void Execute(float length, Action delayedMethod) {
+		Instance.StartCoroutine(_Execute(length, delayedMethod));
+	}
+
+	private static IEnumerator _Execute(float length, Action delayedMethod) {
+		yield return new WaitForSeconds(length);
+		delayedMethod();
+	}
+
+    
+}
