71b37fa3e90fe44577d8b0502feeebe7928353bd
Jonathan Paetzold <jcp.lucky@gmail.com>
2019-01-26 18:07:27 -0800

    cat movement

---
+++ roomba/Assets/Scripts/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveDiagonal(Vector3 v3)
+    {
+        transform.Translate(speed * v3.normalized * Time.deltaTime);
+    }
+}
---
+++ roomba/Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private Cat playerCat;
+    delegate void MovementDelegate(Vector3 v3);
+    MovementDelegate movement;
+
+    private Vector3 v3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCat = (Cat)FindObjectOfType(typeof(Cat));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v3 = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+        movement = MoveDiagonal;
+        MoveDiagonal(v3);
+
+    }
+
+    void MoveDiagonal(Vector3 dir)
+    {
+        playerCat.MoveDiagonal(dir);
+    }
+
+
+
+
+}
