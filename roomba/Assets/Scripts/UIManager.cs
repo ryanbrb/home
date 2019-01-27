d913a702403ab49f95c06d69d23450b5ecf75b0a
Jonathan Paetzold <jcp.lucky@gmail.com>
2019-01-26 21:57:51 -0800

    Controller mapping

--- roomba/Assets/Scripts/UIManager.cs
+++ roomba/Assets/Scripts/UIManager.cs
@@ -1,33 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private Scene currentScene;
+    private string screenName;
     private Cat playerCat;
+    private Vector3 v3;
+    private UITitleScreen titleScreen;
+    public bool menuMove;
+
+
     delegate void MovementDelegate(Vector3 v3);
     MovementDelegate movement;
 
-    private Vector3 v3;
+    delegate void MenuDelegate(string action);
+    MenuDelegate menu;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        playerCat = (Cat)FindObjectOfType(typeof(Cat));
+        menuMove = true;
+        currentScene = SceneManager.GetActiveScene();
+        screenName = currentScene.name;
+
+        if (screenName == "main")
+        {
+            titleScreen = (UITitleScreen)FindObjectOfType(typeof(UITitleScreen));
+        }
+        else
+        {
+            playerCat = (Cat)FindObjectOfType(typeof(Cat));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        v3 = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
-        movement = MoveDiagonal;
-        MoveDiagonal(v3);
+        if (screenName == "main") 
+        {
+            if(Input.GetAxis("Vertical") > 0 && menuMove)
+            {
+                menu = MoveSelection;
+                menu("up");
+                menuMove = false;
+            }
+            else if (Input.GetAxis("Vertical") < 0 && menuMove)
+            {
+                menu = MoveSelection;
+                menu("down");
+                menuMove = false;
+            }
+            else if (Input.GetAxis("Fire1") > 0) {
+                menu = MoveSelection;
+                menu("select");
+            }
+        }
+        else
+        {
+            v3 = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+            movement = MoveDiagonal;
+            MoveDiagonal(v3);
+        }
+
 
     }
 
     void MoveDiagonal(Vector3 dir)
     {
         playerCat.MoveDiagonal(dir);
+    }
+
+    void MoveSelection(string action)
+    {
+        menuMove = true;
+        titleScreen.HandleSelection(action);
     }
 
 
---
+++ roomba/Assets/Scripts/UITitleScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITitleScreen : MonoBehaviour
+{
+
+    public Button[] buttons;
+    public Button currentButton;
+    public int buttonIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = FindObjectsOfType<Button>();
+        if (buttons.Length > 2)
+        {
+            currentButton = buttons[2];
+            buttonIndex = 2;
+            currentButton.Select();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleSelection(string action)
+    {
+        if (action == "up")
+        {
+            Debug.Log("Button Up");
+            try
+            {
+                currentButton = buttons[buttonIndex - 1];
+                buttonIndex -= 1;
+                currentButton.Select();
+            }
+            catch (Exception e)
+            {
+                print("error");
+            }
+        }
+        else if (action == "down")
+        {
+            Debug.Log("Button Down");
+            try
+            {
+                currentButton = buttons[buttonIndex + 1];
+                buttonIndex += 1;
+                currentButton.Select();
+            }
+            catch (Exception)
+            {
+                print("error");
+            }
+        }
+        else if (action == "select")
+        {
+            currentButton.onClick.Invoke();
+        }
+    }
+}
