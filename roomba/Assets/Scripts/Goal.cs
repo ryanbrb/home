5e81b38e50611bee91170435fe0b7949bcde46a7
Michael Pelz <mpelz.lill@gmail.com>
2019-01-26 17:15:15 -0800

    Goal and LevelManager for Level management

--- roomba/Assets/Scripts/Goal.cs
+++ roomba/Assets/Scripts/Goal.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour{
 
-    public GameObject LevelManagerGameObject;
-    public int LevelToLoad = 0;
+    //public GameObject LevelManagerGameObject;
+    //public int LevelToLoad = 0;
 
     void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.tag == "Roomba"){
-            // load next level
-            LevelManagerGameObject.GetComponent<LevelManager>().LoadRoom(LevelToLoad);
+			// load next level
+			//LevelManagerGameObject.GetComponent<LevelManager>().LoadRoom(LevelToLoad);
+			LevelManager.CallEvent(GameEvent.NextLevel);
             Debug.Log("collision detected with roomba");
         }
     }
--- roomba/Assets/Scripts/LevelManager.cs
+++ roomba/Assets/Scripts/LevelManager.cs
@@ -5,30 +5,99 @@ using UnityEngine.SceneManagement;
 
 
 public enum GameEvent {
-BatterDead, Warning
+BatteryDead, Warning, NextLevel
 }
 
 
 public class LevelManager : MonoBehaviour
 {
-	public string[] Level;
-	//public static LevelManager ThisisMe;
+	private static string[] Level = {"Nothing!", "Room01", "Room02"};
+	private static List<string> OtherScenes = new List<string>(new string[] {"main", "credits", "Lose", "Win"});
+
+	private  static int CurrentRoom = 0;
+	private static LevelManager ThisisMe;
 	public void Start()
 	{
-		//LevelManager.ThisisMe = this;
+		LevelManager.ThisisMe = this;
+		
 	}
 	public static void CallEvent(GameEvent EventCalled)
 	{
-		
+		switch (EventCalled)		
+		{
+			case GameEvent.NextLevel:
+				if (LoadNextLevel())
+					return;
+				else
+					Load("main");
+				break;
+			case GameEvent.BatteryDead:
+				Load("main");
+				return;
+				break;
+			case GameEvent.Warning:
+				ThisisMe.Warning();
+				break;
+				
+		}
 	}
-	
-	public void LoadRoom(int RoomNumber){
-        if(Level[RoomNumber] != null) {
-            SceneManager.LoadScene(Level[RoomNumber]);
-            Debug.Log("Loading " + Level[RoomNumber]);
-        }
-        else{
-            Debug.Log("Level does not exist, or is not set in the Level Manager");
-        }
-    }
+
+	private void Warning()
+	{
+		Debug.Log("Warnign");
+	}
+
+	public void LoadRoom(int RoomNumber)
+	{
+		LoadRoomNo(RoomNumber);
+	}
+	private bool LoadRoomNo(int RoomNumber)
+	{
+		if(RoomNumber < Level.Length && RoomNumber >= 0)
+		{
+			if (Level[RoomNumber] != null)
+			{
+				CurrentRoom = RoomNumber;
+				SceneManager.LoadScene(Level[RoomNumber]);
+				Debug.Log("Loading " + Level[RoomNumber]);
+				return true;
+			}
+			else
+			{
+				Debug.Log("Level does not exist, or is not set in the Level Manager");
+				return false;
+			}
+		}
+		Debug.Log("This is bad. can't find " + RoomNumber);
+		return false;
+	}
+	private static bool LoadNextLevel()
+	{
+		CurrentRoom++;
+		return ThisisMe.LoadRoomNo(CurrentRoom);
+	}
+	private static void Load(string scene)
+	{
+		if(OtherScenes.Contains(scene))
+		{
+				SceneManager.LoadScene(scene);
+				Debug.Log("Loading " + scene);
+			/*
+				Debug.Log("Scene \"" + scene + "\" does not exist, or is not in the Level Manager as an optional other scene.");// defaulting to Menu.");
+				if (scene != "main")
+				{
+					Debug.Log("defaulting to Menu.");
+					Load("main");
+				}
+
+			}
+			*/
+		}
+		else
+		{
+			
+			Debug.Log("Scene \"" + scene + "\" does not exist, or is not in the Level Manager as an optional other scene. defaulting to Menu.");
+			Load("main");
+		}
+	}
 }
