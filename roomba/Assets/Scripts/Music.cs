874737ba2ca323699f1a154658b93da8923d37a5
Michael Pelz <mpelz.lill@gmail.com>
2019-01-27 04:15:45 -0800

    Sound Templates
    Please update with coppies of sound code that has been implimented.  and standardize naming conventions.

---
+++ roomba/Assets/Scripts/Music.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	public static Music instance;
+	public enum SceneTypeList { Menu, Game, CutScene }
+
+	[SerializeField]
+	private AudioClip[] mySongs;
+	[SerializeField]
+	private AudioSource source;
+
+
+
+	void Start()
+	{
+		//Singleton
+		if(Music.instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	/// This is called every time a scene is changed.
+	/// the scene Type is what kind of scene it is
+	/// The name is for you to differentiate how to use it - ie sounds for cutscenes
+	/// ---- It is Mike's responsibility for this to work, it is everyone else's responsibility to call these!
+	public void SceneChange(SceneTypeList sceneType, string NameOfScene )
+	{
+		
+	}
+
+}
---
+++ roomba/Assets/Scripts/Sound.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+	
+	public static Sound instance;
+
+	public AudioClip Blah;
+	public AudioClip[] WallHit;
+
+	[SerializeField]
+	private AudioSource[] audioSources;
+
+		// Use get set here
+	private int nextSoundSource = 0;
+
+
+
+
+	public delegate void AfterSound();
+	public AfterSound callWhenDone;
+
+	public enum SoundTrigger { WinSound, LoseSound, MenuSceneChange }
+
+
+
+
+
+
+	/// <summary>
+	/// This is called by all functions that will have a sound effect.
+	/// Ask for everyone else to call this.
+	/// 
+	/// ---- It is Mike's responsibility for this to work, it is everyone else's responsibility to call these!
+	/// </summary>
+	/// <param name="trigger">What triggered this?</param>
+	/// <param name="myDeligate">What happens after. (Can be null)</param>
+	public void MakeSound(SoundTrigger trigger, AfterSound myDeligate)
+	{
+		float lengthOfSound = 0;
+		callWhenDone = myDeligate;
+		AudioClip ClipToPlay = null;
+
+		// here is where you select what sound to make.
+		switch(trigger)
+		{
+			
+		}
+		PlaySound(ClipToPlay);
+		
+	}
+
+
+
+
+
+
+
+	/// <summary>
+	///  This is how you will make a sound
+	/// </summary>
+	/// <param name="ClipToPlay"></param>
+	private void PlaySound(AudioClip ClipToPlay)
+	{
+		audioSources[nextSoundSource].clip = ClipToPlay;
+		audioSources[nextSoundSource].Play();
+		nextSoundSource++;
+		Invoke("OnFinishSound", ClipToPlay.length);
+	}
+
+	/// This is called when the sound is done. if nothing is there we can 
+	public void OnFinishSound()
+	{
+		if(callWhenDone != null)
+		{
+			callWhenDone();
+		}
+	}
+
+
+
+	void Start()
+	{
+		//Singleton
+		if (Music.instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
